7d743522a56cd72490b1392216f463e7afb1ebb9
LinuxCat <masch77.linuxcat@gmail.com>
2023-03-06 17:47:48 +0100

    vector and more instructions

--- lib/logic.hpp
+++ lib/logic.hpp
@@ -1,6 +1,7 @@
 #ifndef _LOGIC_CHIP8
 #define _LOGIC_CHIP8
 #include <stdint.h>
+#include <vector>
 #include "graphics.hpp"
 ////////////////////////////////
 /////////CHIP-8 LOGIC///////////
@@ -10,8 +11,8 @@ uint16_t filesize;
 uint16_t curInst = 0x00E0;
 uint16_t PC = 0x200; //Program Counter. Counts how much instructions we are in
 uint16_t I = 0; //Index register. Used to point at instructions in memory
-uint16_t stack[32] = { 0 };
-uint8_t V0, V1, V2, V3, V4, V5, V6, V7, V8, V9, VA, VB, VC, VD, VE, VF; //registers
+std::vector<uint16_t> stack;
+uint8_t V[15] = {0}; //registers
 uint8_t mem[4096] = { //we fill memory with the font. 
     0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
     0x20, 0x60, 0x20, 0x20, 0x70, // 1
@@ -56,9 +57,47 @@ void FDE(){
             if (curInst == 0x00E0){
                 memset(screen, 0, sizeof(screen));
                 break;
+            } else if (curInst == 0x00EE){
+                PC = stack.back();
+                stack.pop_back();
+            } else {
+                printf("Instruction %x is not known!\n", curInst);
             }
-            
-        
+            break;
+        case 0x1000:
+            PC = 0x0FFF & curInst;
+            break;
+        case 0x2000:
+            stack.push_back(PC);
+            PC = 0x0FFF & curInst;
+        case 0x3000:
+            printf("Instruction %x is not implemented!\n", curInst);
+        case 0x4000:
+            printf("Instruction %x is not implemented!\n", curInst);
+        case 0x5000:
+            printf("Instruction %x is not implemented!\n", curInst);
+        case 0x6000:
+            V[(curInst & 0x0F00) >> 8] = curInst & 0x00FF;
+        case 0x7000:
+            V[(curInst & 0x0F00) >> 8] += curInst & 0x00FF;
+        case 0x8000:
+            printf("Instruction %x is not implemented!\n", curInst);
+        case 0x9000:
+            printf("Instruction %x is not implemented!\n", curInst);
+        case 0xA000:
+
+        case 0xB000:
+            printf("Instruction %x is not implemented!\n", curInst);
+        case 0xC000:
+            printf("Instruction %x is not implemented!\n", curInst);
+        case 0xD000:
+
+        case 0xE000:
+            printf("Instruction %x is not implemented!\n", curInst);
+        case 0xF000:
+            printf("Instruction %x is not implemented!\n", curInst);
+        default:
+            printf("What is your system?!\n")
     }
 }
 #endif //_LOGIC_CHIP8
